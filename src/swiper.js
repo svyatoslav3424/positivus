5f2d6ebdc2bf0336d6b0d3978c9ca445c6c966c1
svyatoslav3424 <zaitsev.svyatoslav@gmail.com>
2025-06-18 11:38:53 +0300

    add use cases page & implement adaptive styles for home page

--- src/swiper.js
+++ src/swiper.js
@@ -1,5 +1,19 @@
 const swiper1 = new Swiper(".swiper-1", {
-  slidesPerView: 6,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.5,
+    },
+    380: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 6,
+    },
+  },
+
   loop: true,
   freeMode: true,
   speed: 1200,
@@ -10,10 +24,22 @@ const swiper1 = new Swiper(".swiper-1", {
 });
 
 const swiper2 = new Swiper(".swiper-2", {
+  breakpoints: {
+    0: {
+      slidesPerView: 1.5,
+    },
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+  },
+
   slidesPerView: 2,
   // loop: true,
   freeMode: true,
-
+  spaceBetween: 30,
   speed: 4000,
   autoplay: {
     delay: 1000,
