777f1458e914b5fafab387969146bb2407124ff2
svyatoslav3424 <zaitsev.svyatoslav@gmail.com>
2025-07-09 12:04:44 +0300

    add animation styles

---
+++ src/use-cases.js
@@ -0,0 +1,23 @@
+const swiper3 = new Swiper(".swiper-3", {
+  breakpoints: {
+    0: {
+      slidesPerView: 1.5,
+    },
+    320: {
+      slidesPerView: 1,
+    },
+    1360: {
+      slidesPerView: 2,
+    },
+  },
+
+  slidesPerView: 2,
+  // loop: true,
+  freeMode: true,
+  spaceBetween: 30,
+  speed: 4000,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+});
